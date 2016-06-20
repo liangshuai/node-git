7fc7cfb42c2a90c5ce4b7ce4cae27a7518f0a095
Shuai_Liang <Shuai_Liang@epam.com>
2016-06-20 15:31:00 +0800

    git show

--- src/command/index.js
+++ src/command/index.js
@@ -5,9 +5,11 @@ var log = require('./log');
 var files = require('./ls-files');
 var diff = require('./diff');
 var clone = require('./clone');
+var show = require('./show');
 
 module.exports.status = status;
 module.exports.log = log;
 module.exports.files = files;
 module.exports.diff = diff;
 module.exports.clone = clone;
+module.exports.show = show;
